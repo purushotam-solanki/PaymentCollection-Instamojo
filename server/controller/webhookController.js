96f6ab0391a93a69b6cdd37000ab01284b02dd22
Purushotam <92419272+purushotam-solanki@users.noreply.github.com>
2023-03-15 02:23:51 +0530

    Update webhookController.js

--- server/controller/webhookController.js
+++ server/controller/webhookController.js
@@ -6,7 +6,7 @@ const { Invoices } = require("../database/schema/Invoice")
 router.post("/instamojo", async (req, res) => {
     try {
         console.log("webhook request: " + req.body)
-        let status = VALID_STATUS.includes(req.body.status.toLowerCase()) ? "success" : data.status.toLowerCase();
+        let status = VALID_STATUS.includes(req.body.status.toLowerCase()) ? "paid" : data.status.toLowerCase();
         await Invoices.findOneAndUpdate({ paymentId: req.body.payment_request_id }, { $set: { status: status } })
         console.log("webhook end point hit")
         res.status(200).end()
