34e84d3e05d7ffe4a7632b2cfdab27426205903e
Purushotam Solanki <psolanki0004@gmail.com>
2023-03-14 16:39:21 +0530

    feat: moving client to server for deployment

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const cors = require("cors")
 const bodyParser = require('body-parser')
 const Insta = require("instamojo-nodejs")
+const path = require("path")
 
 //Seprating internal imports from external imports
 const paymentsController = require("./controller/paymentsController")
@@ -23,7 +24,6 @@ app.use(bodyParser.json())
 //Setting keys for InstaMojo
 Insta.setKeys(process.env.INSTAMOJO_API_KEY, process.env.INSTAMOJO_AUTH_KEY);
 Insta.isSandboxMode(true);
-
 app.get("/", (req, res) => {
     res.send("Server is Running!!!")
 })
@@ -35,6 +35,12 @@ app.post("/webhook", async (req, res) => {
     await Payments.findOneAndUpdate({ providerId: data.id }, { $set: { status: data.status } })
     console.log("webhook end point hit")
 })
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client', 'build')))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 app.listen(PORT, () => {
