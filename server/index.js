edf9fbc072abb5140ca646249eb15c101b8d3169
Purushotam <92419272+purushotam-solanki@users.noreply.github.com>
2023-03-15 03:00:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.use(cors({
     origin: [`${process.env.CLIENT_BASE_URL}`]
 }))
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //Setting keys for InstaMojo
